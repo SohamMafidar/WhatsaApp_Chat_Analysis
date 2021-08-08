80f15c51bc9686598669529f48d059b02de239de
SohamMafidar <66075716+SohamMafidar@users.noreply.github.com>
2021-08-08 16:34:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+""" Importing all the libraries which may come in use for current file and future branching """
+
 import regex as re
 import pandas as pd
 import numpy as np
@@ -10,12 +13,18 @@ from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 % matplotlib inline
 
+""" FOLLOWING FUNCTIONS PRE-PROCESS THE DATA SO THAT ANALYSIS CAN BE CARRIED OUT"""
+
+# This function breaks the text into date & time using regex
+
 def StartsWithDate(s):
     pattern = '^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]):([0-9][0-9]) -'
     result = re.match(pattern,s)
     if result:
         return True
     return False
+
+# This function finds out the author of every message being sent
 
 def StartsWithAuthor(s):
    patterns = ['([\w]+):', '([\w]+[\s]+[\w]+):', '([\w]+[\s]+[\w]+[\s]+[\w]+):']
@@ -25,27 +34,29 @@ def StartsWithAuthor(s):
        return True
    return False
 
+# The real deal. This function gets the message from the text file
 
 def getDataPoint(line):
-    splitLine = line.split(' - ')  # splitLine = ['18/06/17, 22:47', 'Loki: Why do you have 2 numbers, Banner?']
-
+    splitLine = line.split(' - ')  # splitLine = ['18/06/17, 22:47', 'Hi!! Welcome to group]
     dateTime = splitLine[0]  # dateTime = '18/06/17, 22:47'
 
     date, time = dateTime.split(', ')  # date = '18/06/17'; time = '22:47'
 
-    message = ' '.join(splitLine[1:])  # message = 'Loki: Why do you have 2 numbers, Banner?'
+    message = ' '.join(splitLine[1:])  # message = 'Baba: Who's up for lunch?'
 
     if StartsWithAuthor(message):  # True
-        splitMessage = message.split(': ')  # splitMessage = ['Loki', 'Why do you have 2 numbers, Banner?']
-        author = splitMessage[0]  # author = 'Loki'
-        message = ' '.join(splitMessage[1:])  # message = 'Why do you have 2 numbers, Banner?'
+        splitMessage = message.split(': ')  # splitMessage = ['Baba', ' Who's up for lunch?']
+        author = splitMessage[0]  # author = 'Baba'
+        message = ' '.join(splitMessage[1:])  # message = ' Who's up for lunch?'
     else:
         author = None
     return date, time, author, message
 
+""" MAIN STARTS FROM HERE (LINE 48) """
 
 parsed_data = []
-path = '/content/gtm.txt'
+
+path = '/content/gtm.txt'   # Path of file.
 with open(path, encoding="utf-8") as f:
     f.readline()
     buffer = []
@@ -64,30 +75,42 @@ with open(path, encoding="utf-8") as f:
         else:
             buffer.append(line)
 
+""" THE DATA NOW IS READY TO BE PROCESSED.
+
+FOLLOWING LINES WOULD MAKE DATA FRAME AND START ANALYSIS AND PLOTTING ON IT. """
+
 #  Making a pandas data frame
 df = pd.DataFrame(parsed_data, columns=['Date', 'Time', 'Author', 'Message'])
 df.head(10)
 
 df.describe()
 
+# MOST TALKATIVE MEMBERS 
 author_value_counts = df['Author'].value_counts()
 top_3_author = author_value_counts.head(3)
 top_3_author.plot.barh();
+
+# PEOPLE WHO SEND MOST MEDIA (INCREASING ORDERS)
 
 media_df = df[df['Message'] == '<Media omitted>']
 media_count = media_df['Author'].value_counts()
 top_3_media = media_count.head(3)
 top_3_media.plot.barh();
 
+# NO.OF LETTERS AND WORDS BY EACH MEMBER 
 
 df['Letter Count'] = df['Message'].apply(lambda s: len(s))
 df['Word Count'] = df['Message'].apply(lambda s: len(s.split(' ')))
 print(df)
 
+# FINDING DISCRETE AND CONITNOUS DATA
+
 discrete_columns = [['Date', 'Time', 'Author', 'Message']]
 df[discrete_columns].describe()
 continous_columns = [['Letter Count', 'Word Count']]
 df[continous_columns].describe()
+
+# MOST COMMON WORDS
 
 plt.figure(figsize=(15, 2))
 common_words = df['Word Count'].value_counts()
@@ -96,6 +119,7 @@ top_10_used.plot.bar()
 plt.xlabel('Word Count')
 plt.ylabel('Frequency')
 
+# DAYS ON WHICH MOST MESSAGES WERE SENT.
 
 df['Date'].value_counts().head(5).plot.barh();
 plt.xlabel('Number of Messages');
